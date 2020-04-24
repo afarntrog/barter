475f95302987b225c8a03aab1e6efd3e93886276
AaronF <aaronfarntrog@gmail.com>
2020-04-24 18:40:35 -0400

    added websocket chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,15 +1,28 @@
 # chat/consumers.py
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
+        """
+        :at self.room_name Obtains the 'room_name' parameter from the URL route in chat/routing.py that opened the WebSocket connection to the consumer.
+                Every consumer has a scope that contains information about ITS OWN connection
+        """
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'{self.room_name}'
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
+        """
+        Parameters
+        ----------
+        text_data : str
+        
+
+        """
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         print(text_data_json)
--- core/models.py
+++ core/models.py
@@ -9,13 +9,41 @@ from taggit.managers import TaggableManager
 from taggit.models import GenericUUIDTaggedItemBase, TaggedItemBase
 
 
+class BaseModel(models.Model):
+    """
+    Base model to store bare fields used in many models.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+    def hard_delete(self, *args, **kwargs):
+        """ Completely delete object forever """
+        super().delete()
+
+    def soft_delete(self, *args, **kwargs):
+        """
+        Soft deletes object. Like hiding model.
+        Call hard_delete() to completely delete object.
+        """
+        if self.is_deleted: return
+        self.is_deleted = True
+        self.deleted_at = datetime.now()
+        self.save()
+
+
 # If you only inherit GenericUUIDTaggedItemBase, you need to define  a tag field.
 #  e.g. tag = models.ForeignKey(Tag, related_name="uuid_tagged_items", on_delete=models.CASCADE)
 class UUIDTaggedItem(GenericUUIDTaggedItemBase, TaggedItemBase):
-
     class Meta:
         verbose_name = "Tag"
         verbose_name_plural = "Tags"
+
 
 # Stores info about the listed product
 class Product(models.Model):
@@ -35,10 +63,12 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse("product_detail", args=[str(self.uid)])
 
+
 # Many to one: Stores images for each product
 class ProductImages(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_images')
     image = models.ImageField(upload_to='product_images/')
+
 
 # Count how many times the product was viewed
 class ProductViewCount(models.Model):
@@ -46,7 +76,9 @@ class ProductViewCount(models.Model):
     ip = models.CharField(max_length=40)
     created = models.DateTimeField(auto_now_add=True)
 
+
 # Stores Favorite list
 class Favorite(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products_favorites') # Use this related_name if you want to get all the users who favorited a specific product
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                related_name='products_favorites')  # Use this related_name if you want to get all the users who favorited a specific product
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_favorites')
